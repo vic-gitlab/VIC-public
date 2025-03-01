191d0dad60d3ccef2fb5a010b4d574365f244bb9
vic-gitlab <vicgitlab@gmail.com>
2025-03-01 23:44:51 +0100

    Update Almalinux-docker-install.sh

--- Almalinux-docker-install.sh
+++ Almalinux-docker-install.sh
@@ -9,6 +9,12 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 # Install Docker and required components
 dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
+# Start docker on boot
+systemctl enable docker
+
+# Start docker
+systemctl start docker
+
 # Add vicuser to docker group
 usermod -aG docker vicuser
 
